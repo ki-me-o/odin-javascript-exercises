ebba6087d16964dca7c7d1ac5ca53c384924a084
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 11:50:21 -0600

    Add check and new return value for negative num value

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,11 @@
 const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+
     let repetitiveString = "";
     for (let i = 0; i < num; i++) {
         repetitiveString += string;
     }
+    
     return repetitiveString;
 };
 
