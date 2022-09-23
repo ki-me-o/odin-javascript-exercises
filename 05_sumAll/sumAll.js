3022072c465968cfdfbb63d892dfd9a040250f55
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 16:46:33 -0600

    Add functionality to sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(lowEnd,highEnd) {
+    let sum = 0;
+    for(let i = lowEnd; i <= highEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
