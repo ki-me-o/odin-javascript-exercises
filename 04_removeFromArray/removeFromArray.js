4424a65002b03b6c78add8dcb7326d4ba55be308
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 16:35:47 -0600

    Add functionality to removeFromArray to remove prescribed entries

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removals) {
+    console.log("removals = " + removals)
+    console.log("removals.length = " + removals.length)
+    console.log("removals[0] = " + removals[0])
+    
+    for(let i = 0; i < removals.length; i++) {
+        // Traversing array backwards to avoid affecting upcoming array indices
+        for(let j = array.length - 1; j >= 0; j--) {
+            if(removals[i] === array[j]) {
+                array.splice(j,1);
+            }
+        }
+    }
 
+    return array;
 };
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
