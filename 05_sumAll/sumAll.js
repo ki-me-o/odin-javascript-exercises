a99f4d2cb6b1da87094f13725d9116a3acb4dbe8
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 16:51:38 -0600

    Continue tests and fix bug where larger number had to be second argument

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function(lowEnd,highEnd) {
+const sumAll = function(num1,num2) {
     let sum = 0;
+    let lowEnd, highEnd;
+    if (num2 > num1) {
+        lowEnd = num1;
+        highEnd = num2;
+    } else {
+        lowEnd = num2;
+        highEnd = num1;
+    }
+
     for(let i = lowEnd; i <= highEnd; i++) {
         sum += i;
     }
