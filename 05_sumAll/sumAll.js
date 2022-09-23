2fed3695cd74525722b2eb228ec776448453ecd2
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 17:03:57 -0600

    Add input checking for non-number values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(num1,num2) {
     let sum = 0;
     let lowEnd, highEnd;
-    if( num1 < 0 || num2 < 0) return "ERROR"
+    if( verifyNumber(num1) == "ERROR" || verifyNumber(num2) == "ERROR") return "ERROR"
 
     if (num2 > num1) {
         lowEnd = num1;
@@ -17,5 +17,11 @@ const sumAll = function(num1,num2) {
     return sum;
 };
 
+function verifyNumber(num) {
+    if(num < 0) return "ERROR";
+    if(isNaN(num)) return "ERROR";
+    if(num === num.toString()) return "ERROR";
+    return "VERIFIED"
+}
 // Do not edit below this line
 module.exports = sumAll;
