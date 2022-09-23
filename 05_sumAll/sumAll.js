467ff47ea90ca700ab495f9639078709600ad18d
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 16:53:39 -0600

    Add error statement if negative number is passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function(num1,num2) {
     let sum = 0;
     let lowEnd, highEnd;
+    if( num1 < 0 || num2 < 0) return "ERROR"
+
     if (num2 > num1) {
         lowEnd = num1;
         highEnd = num2;
