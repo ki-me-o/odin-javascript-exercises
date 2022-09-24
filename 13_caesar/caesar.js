d64a8e87afc56a7eb571cfe94bf968389be1261a
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-24 14:19:25 -0600

    Add helper function shiftChar that checks input and shifts single char

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,29 @@
-const caesar = function() {
-
+const caesar = function(message, shift) {
+    
 };
 
+function shiftChar(char, shift) {
+    // Check case - A-Z = 65-90, a-z = 97-122 unicode values
+    const UPPERCASE_OFFSET = 65;
+    const LOWERCASE_OFFSET = 97;
+    const ALPHABET_SIZE = 26;
+
+    const alphaPattern = /[^a-zA-Z]/;
+    if(alphaPattern.test(char)) return char;
+
+    const lowercasePattern = /[a-z]/;
+    let unicodeOffset = lowercasePattern.test(char) ? LOWERCASE_OFFSET : UPPERCASE_OFFSET;
+
+    let charValue = char.charCodeAt();
+
+    return String.fromCharCode(modulo(((charValue - unicodeOffset) + shift), ALPHABET_SIZE) + unicodeOffset);
+}
+
+function modulo (a,b) {
+    return ((a % b) + b) % b;
+}
+
+
+
 // Do not edit below this line
-module.exports = caesar;
+//module.exports = caesar;
