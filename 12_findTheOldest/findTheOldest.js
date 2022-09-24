c82797b4b95a546bcf4f769b7ead170ea12dad7b
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-24 13:20:42 -0600

    Loop through people to find oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const d = new Date();
+    let oldestPerson;
+    let oldestAge = 0;
+    people.forEach(person => {
+        let age =   (person.yearOfDeath != undefined) ?
+                    (person.yearOfDeath - person.yearOfBirth):
+                    (d.getFullYear() - person.yearOfBirth);
+        if(oldestPerson == undefined || age > oldestAge) {
+            oldestPerson = person;
+            oldestAge = age;
+        } 
+    });
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
