1629bb2ced642256a5b0b9e6e5894c66040ea85e
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 19:32:45 -0600

    Add functionality to ftoc and ctof functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+// C = (F - 32) x (5/9)
+const ftoc = function(fahrenheit) {
+  return (fahrenheit - 32) * (5/9);
 };
 
-const ctof = function() {
-
+// F = C x (9/5) + 32
+const ctof = function(celsius) {
+  return celsius * (9/5) + 32;
 };
 
 // Do not edit below this line
