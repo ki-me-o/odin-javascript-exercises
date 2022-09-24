db9e311486c15a4ed095775b306161f62906e3bf
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 20:59:02 -0600

    Convert string inputs to numbers and disallow negatives

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
 const fibonacci = function(sequence, twoAgo = 1, oneAgo = 1, currentSequence = 3) {
-    if(sequence <= 2) return 1;
+    sequence = Number(sequence);
+    if(sequence < 0) return "OOPS";
+    else if(sequence <= 2) return 1;
     const currentValue = twoAgo + oneAgo;
     return (currentSequence == sequence) ? currentValue : fibonacci(sequence, oneAgo, currentValue, currentSequence + 1);
 };
