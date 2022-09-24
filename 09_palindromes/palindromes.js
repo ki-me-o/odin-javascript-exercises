e0bf13fbb09f2c281c44d2893fb9bd9653caae32
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-23 20:07:50 -0600

    Add initial functionality to palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    cleanString = removeSeparators(string);
+    return (cleanString === reverseString(cleanString));
 };
+
+function removeSeparators(string) {
+    return string.replaceAll(/[^a-zA-Z0-9]/mg, "");
+}
+
+function reverseString(string) {
+    let reversedString = "";
+    for(let i = string.length - 1; i >= 0; i--) {
+        reversedString += string.charAt(i);
+    }
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
